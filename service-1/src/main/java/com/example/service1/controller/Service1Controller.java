9d9e72554cf5d894998b8407c3e5821586ece817
unknown <a01092948620@gmail.com>
2022-02-16 09:54:52 +0900

    git-test

--- service-1/src/main/java/com/example/service1/controller/Service1Controller.java
+++ service-1/src/main/java/com/example/service1/controller/Service1Controller.java
@@ -20,9 +20,14 @@ public class Service1Controller {
     public String status(){
         return String.format("It's Working in User Service"
                 +", port(local.server.port)="+env.getProperty("local.server.port")
-                +", port(server.port)="+env.getProperty("server.port")
-                +", token secret="+env.getProperty("token.secret")
-                +", token expiration time="+env.getProperty("token.expiration_time"));
+                + " 1. git clone [주소: shift+insert]"
+                +  "git branch junho"
+                +"git checkout junho"
+                +"2. git add . => 전체 변경한 내용이 전부 올라가는것."
+                +"2. git add Service1Controller.java => 옆에 있는 파일만 local에 저장"
+                +"3. git commit -m 'servicecontroller 변경'"
+                +"4. git push origin junho"
+                );
     }
 
 
